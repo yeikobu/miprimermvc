b1d7c181c6dd3cbe42a84c6b52ad7db3bf64e1b7
yeikobu <aguilar.campos.jacob@gmail.com>
2024-05-05 00:26:00 -0400

    Change some code format

--- Controller/ControllerSearchJugador.php
+++ Controller/ControllerSearchJugador.php
@@ -7,7 +7,7 @@ if (empty($_POST['nombre'])) {
 
 include('../Model/ModelCRUDJugador.php');
 $objugador = new ModelCRUDJugador();
-$respuesta = $objugador->Searchjugador($nombre);
+$respuesta = $objugador -> Searchjugador($nombre);
 foreach ($respuesta as $value) {
   echo $value['RUT_JUGADOR'] . " " . $value['VCH_DV_JUGADOR'] . " " . $value['VCH_NOMBRE_JUGADOR'] . " " . $value['VCH_APELLIDO_JUGADOR'] . " " . $value['DATE_FECHA_NACIMIENTO_JUGADOR'] . " " . $value['VCH_MAIL_JUGADOR'] . " " . $value['VCH_ALIAS_JUGADOR'] . " " . $value['VCH_NACIONALIDAD_JUGADOR'] . "<br>";
 }
--- Model/ModelCRUDJugador.php
+++ Model/ModelCRUDJugador.php
@@ -1,9 +1,7 @@
 <?php
 include('Conexion.php');
-class ModelCRUDJugador
-{
-    public function insertJugador($rut, $dv, $nombre, $fecnac, $apellido, $alias, $mail, $nacionalidad)
-    {
+class ModelCRUDJugador {
+    public function insertJugador($rut, $dv, $nombre, $fecnac, $apellido, $alias, $mail, $nacionalidad) {
         $query = "INSERT INTO `tbl_jugador`(`rut_jugador`, `vch_dv_jugador`, `vch_nombre_jugador`, `vch_apellido_jugador`, `date_fecha_nacimiento_jugador`, `vch_mail_jugador`, `vch_alias_jugador`, `vch_nacionalidad_jugador`) VALUES ('$rut','$dv','$nombre','$fecnac','$apellido','$alias','$mail','$nacionalidad')";
         $men = "";
         $streamConnection = Conexion::comenzarConexion();
@@ -22,8 +20,7 @@ class ModelCRUDJugador
         return $men;
     }
 
-    public function Searchjugador($nombre)
-    {
+    public function Searchjugador($nombre) {
         $query = "SELECT * FROM tbl_jugador WHERE VCH_NOMBRE_JUGADOR LIKE '%$nombre%'";
         $stringconnection = Conexion::comenzarConexion();
         $result = mysqli_query($stringconnection, $query);
